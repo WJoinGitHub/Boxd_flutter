fa9cac691146ff3b16a0cec67c5b5f257509bb06
wangxianghua <wangxianghua@tenclass.com>
2026-02-07 18:55:30 +0800

    1.0.1.260207182--去掉第三方登录的sdk

--- lib/pages/login/login_options_page.dart
+++ lib/pages/login/login_options_page.dart
@@ -1,12 +1,7 @@
-import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 import 'package:flutter_boxd_app_flow/gen/assets.gen.dart';
-import 'package:flutter_boxd_app_flow/services/google_sign_in_service.dart';
-import 'package:flutter_boxd_app_flow/services/apple_sign_in_service.dart';
-import 'package:flutter_boxd_app_flow/services/facebook_sign_in_service.dart';
 import 'package:flutter_boxd_app_flow/pages/login/email_login_page.dart';
 import 'package:flutter_boxd_app_flow/utils/bx_app_bar.dart';
-import 'package:flutter_boxd_app_flow/widgets/social_button.dart';
 import 'package:flutter_boxd_app_flow/l10n/app_localizations.dart';
 import 'register_email_page.dart';
 
@@ -50,31 +45,6 @@ class _LoginRoot extends StatelessWidget {
               child: Center(child: Text(l10n.t('email_login'))),
             ),
           ),
-          const SizedBox(height: 10),
-          SocialButton(
-            text: l10n.t('facebook_login'),
-            icon: Icons.facebook,
-            onPressed: () async {
-              await FacebookSignInService.signIn(context);
-            },
-          ),
-          const SizedBox(height: 10),
-          if (Platform.isAndroid)
-            SocialButton(
-              text: l10n.t('google_login'),
-              icon: Icons.g_mobiledata,
-              onPressed: () async {
-                await GoogleSignInService.signIn(context);
-              },
-            ),
-          if (Platform.isIOS)
-            SocialButton(
-              text: l10n.t('apple_login'),
-              icon: Icons.apple,
-              onPressed: () async {
-                await AppleSignInService.signIn(context);
-              },
-            ),
           const SizedBox(height: 17),
           TextButton(
             onPressed: () {
